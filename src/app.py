b4a1fd14bf11a75bc77745b93113a8b88af8c6c7
NickOsipov <nick.osipov.91@gmail.com>
2026-03-02 21:35:25 +0300

    feat(app): implement healthcheck and prediction endpoints

---
+++ src/app.py
@@ -0,0 +1,49 @@
+"""
+Module: app.py
+"""
+
+from flask import Flask, jsonify, request
+from loguru import logger
+import pandas as pd
+
+from config.variables import MODEL_PATH, IRIS_CLASSES, FEATURE_NAMES
+from src.inference import load_model, predict
+
+
+app = Flask(__name__)
+
+logger.info(f"Loading model from {MODEL_PATH}...")
+MODEL = load_model(MODEL_PATH)
+logger.info("Model loaded successfully.")
+
+
+@app.route("/")
+def healthcheck():
+    """
+    Healthcheck endpoint.
+    """
+    return jsonify({"status": "ok"})
+
+
+@app.route("/predict", methods=["POST"])
+def prediction():
+    """
+    Prediction endpoint.
+    Expects a JSON payload with the features for prediction.
+    """
+    logger.info("Receiving prediction request.")
+
+    try:
+        data = request.get_json()
+        logger.debug(f"Request data: {data}")
+        df = pd.DataFrame(data, index=[0])
+        logger.info("DataFrame prepared for prediction.")
+        pred = predict(MODEL, df[FEATURE_NAMES])
+        logger.info(f"Prediction result: {pred}")
+        predicted_class = IRIS_CLASSES[pred[0]]
+        logger.info(f"Predicted class: {predicted_class}")
+        logger.info("Sending prediction response!")
+        return jsonify({"predicted_class": predicted_class})
+    except Exception as e:
+        logger.error(f"Error during prediction: {e}")
+        return jsonify({"error": str(e)}), 555
