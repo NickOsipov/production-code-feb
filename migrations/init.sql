a311783cd55bf6868e07692fbc7b3281f43fad13
NickOsipov <nick.osipov.91@gmail.com>
2026-03-13 08:04:35 +0300

    feat(migrations): add initial database and predictions table setup

---
+++ migrations/init.sql
@@ -0,0 +1,16 @@
+-- Active: 1773339318353@@127.0.0.1@3306@mysql
+CREATE DATABASE IF NOT EXISTS `machine_learning`;
+
+USE `machine_learning`;
+
+CREATE TABLE IF NOT EXISTS `predictions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sepal_length` FLOAT NOT NULL,
+  `sepal_width` FLOAT NOT NULL,
+  `petal_length` FLOAT NOT NULL,
+  `petal_width` FLOAT NOT NULL,
+  `predicted_class` VARCHAR(255) NOT NULL,
+  `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
